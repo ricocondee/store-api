d6d0522febac30585720b34add96459ef84d387c
Emanuel Rico Conde <ricocondee@gmail.com>
2022-12-31 18:12:48 -0500

    fixed urls

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get("/api/products", (req, res) => {
   db.all(query.SELECTALL, (error, data) => {
