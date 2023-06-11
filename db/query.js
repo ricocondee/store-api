9852d232290883d4c18a5d01075cfaf2fb509584
Emanuel Rico Conde <ricocondee@gmail.com>
2023-06-11 12:30:49 -0500

    using LIKE operator

--- db/query.js
+++ db/query.js
@@ -4,6 +4,7 @@ const query = {
     SELECTBYTYPE: "SELECT p.* FROM products p INNER JOIN typeOf t ON t.id = p.type AND t.name = ?",
     SELECTCAT: "SELECT * FROM categories",
     SELECTBRANDS: "SELECT * FROM brands",
+    SELECTLIKE: "SELECT * FROM products WHERE name LIKE ?",
   };
   
   module.exports = query;
